8db171a79e8efd96970b5e2f21be2581ad01578b
damlakaynarca <109604901+damlakaynarca@users.noreply.github.com>
2023-11-08 09:29:05 +0300

    Add files via upload

---
+++ webApplicationss/src/main/java/com/example/webapplicationss/WebApplicationssApplication.java
@@ -0,0 +1,13 @@
+package com.example.webapplicationss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationssApplication.class, args);
+    }
+
+}
---
+++ webApplicationss/src/test/java/com/example/webapplicationss/WebApplicationssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapplicationss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
